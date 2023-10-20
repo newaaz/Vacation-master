93a0458280bfbdc9b9c62def03d2d99165e7d8e6
jumper <jumperka@list.ru>
2023-10-20 21:14:13 +0300

    Admin can see list of all vacations

---
+++ spec/features/vacation/index_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+feature 'The admin can view the list of vacation requests', %q{
+  In order to accept and reject a vacation requests
+  As an admin
+  I'd like to be able to to a list of all vacations
+} do
+
+  given!(:vacations) { create_list(:vacation, 3) }
+  given(:admin)      { create(:employee, admin: true) }
+  given(:employee)   { create(:employee) }
+
+  scenario 'Authenticated admin view list of all vacations' do
+    sign_in_employee(admin)
+    visit vacations_path
+
+    vacations.each do |vacation|
+      expect(page).to have_link("Vacation request № #{vacation.id}")
+      expect(page).to have_content("from #{vacation.start_date} to #{vacation.end_date}")
+    end
+  end
+
+  scenario 'Unauthenticated user tries to view list of all vacations' do
+    visit root_path
+    expect(page).to_not have_link('Vacations')
+  end
+
+  scenario 'Authenticated employee tries to view list of all vacations' do
+    sign_in_employee(employee)
+    visit root_path
+    expect(page).to_not have_link('Vacations')
+  end
+end
